8be6baa411c2803951df729da0d9fe9085246f7c
ljanu <info@gamatall.cz>
2021-08-27 12:49:14 +0200

    first commit

---
+++ src/StartServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+
+namespace Tudy\Start;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class StartServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+
+        //register routs
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+
+    }
+
+
+    public function register()
+    {
+
+    }
+
+}
---
+++ src/routes/web.php
@@ -0,0 +1,5 @@
+<?php
+
+Route::get('/start', function () {
+    return 'Hello START Packages';
+})->name('start');
