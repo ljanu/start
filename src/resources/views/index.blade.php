4a05dc5c192643df92bca91da733a4fddd540ead
ljanu <info@gamatall.cz>
2021-08-29 17:39:04 +0200

    correct the name of the called component

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,11 +1,13 @@
 {{-- view index --}}
-<x-start::start.layout.follow title="Skeleton"
-                        description="home page skeleton"
-                        canonical="https://localkost">
+<x-start.layout.follow title="Skeleton"
+                       description="home page skeleton"
+                       canonical="https://localkost">
+
     <x-slot name="content">
         <h1>skeleton</h1>
     </x-slot>
 
-</x-start::start.layout.follow>
+</x-start.layout.follow>
+
 
 
