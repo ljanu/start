9fe109a8cd71350707a36f541d69e0ed1259de66
ljanu <info@gamatall.cz>
2021-09-01 09:21:38 +0200

    added component registration

--- src/resources/views/components/start/layout/welcome.blade.php
+++ src/resources/views/components/start/layout/welcome.blade.php
@@ -78,6 +78,22 @@
 {{-- google tagmaneger ---------------------------------------------------------------------------}}
 {{$googleTagManager ?? null}}
 
+{{-- login --}}
+
+@if (Route::has('login'))
+    <div class=" fixed top-0 right-0 px-6 py-4 sm:block">
+        @auth
+            <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">{{trans("Dashboard")}}</a>
+        @else
+            <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+            @endif
+        @endauth
+    </div>
+@endif
+
 {{-- main content --------------------------------------------------------------------------------}}
 <div class = " bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 min-h-screen " >
 
