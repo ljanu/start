f68661d34d63700382124ce227532c2bb31d77e3
ljanu <info@gamatall.cz>
2021-09-01 09:43:50 +0200

    update html

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -4,10 +4,13 @@
                        canonical="https://localkost">
 
     <x-slot name="content">
+
         <h1 >Start</h1 >
 
-        <a href = "{{route("start")}}" >základní typografie</a >
-        <a href = "{{route("form")}}" >formulář</a >
+        <ul >
+            <li ><a href = "{{route("start")}}" >základní typografie</a ></li >
+            <li ><a href = "{{route("form")}}" >formulář</a ></li >
+        </ul >
 
     </x-slot>
 
