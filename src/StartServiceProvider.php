da8bd96e8b28f671ec43f36b2d390dae4ced9101
ljanu <info@gamatall.cz>
2021-09-01 12:27:26 +0200

    removed name published function

--- src/StartServiceProvider.php
+++ src/StartServiceProvider.php
@@ -23,41 +23,39 @@ class StartServiceProvider extends ServiceProvider
         //register translate
         $this->loadTranslationsFrom(__DIR__ . '/resources/lang', 'start');
 
+
+        //-------- PUBLISHES --------------------------------------------------
+
         //publishes layout
         $this->publishes([
             __DIR__ . '/resources/views/components/start'
             => resource_path('views/components/start'),
-        ],
-            'start-layout');
+        ]);
 
         // published index
         $this->publishes([
             __DIR__ . '/resources/views/index.blade.php'
             => resource_path('views/index.blade.php'),
-        ],
-            'start-index');
+        ]);
 
         // published dashboard
         $this->publishes([
             __DIR__ . '/resources/views/dashboard.blade.php'
             => resource_path('views/dashboard.blade.php'),
-        ],
-            'start-dashboard');
+        ]);
 
 
         // published css
         $this->publishes([
             __DIR__ . '/resources/css/start.css'
             => resource_path('css/start.css'),
-        ],
-            'start-css');
+        ]);
 
         //publish translate
         $this->publishes([
             __DIR__.'/resources/lang'
             => resource_path('lang'),
-        ],
-            'start-translate');
+        ]);
 
     }
 
